c26c078c87f33164937cdb13522bf83155aadb07
Anna Jarosz <annajarosz1310@gmail.com>
2022-04-19 19:59:59 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "kwiecien";
+        int year = 2022;
+        long day = 19;
+        System.out.println("miesiac:" + month);
+        System.out.println("rok:" + year);
+        System.out.println("dzien:" + day);
+    }
+}
+
